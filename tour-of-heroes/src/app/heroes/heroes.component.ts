3548cbcc744f50803e97be00117b610b650e3f79
Harsh Vardhan Sharma <hvsharma_learning@outlook.com>
2018-01-07 00:08:37 +0530

    Added Heroes Component

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../heroes/hero';
+import { HEROES } from '../heroes/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -9,10 +10,12 @@ import { Hero } from '../heroes/hero';
 
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm';
-  };
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
