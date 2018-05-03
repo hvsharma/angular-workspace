1e7b6665ba8d6e83eaf35465fb35860aa970a217
Harsh V Sharma <hvsharma_learning@outlook.com>
2018-05-03 19:10:59 +0530

    Tinker

--- mps-portal/src/app/model/role.ts
+++
@@ -1,7 +0,0 @@
-export class Role {
-
-  roleGUID: string;
-  roleName: string;
-  roleShortName: string;
-
-}
--- tinkering/e2e/app.e2e-spec.ts
+++ tinkering/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('mps-portal App', () => {
+describe('tinkering App', () => {
   let page: AppPage;
 
   beforeEach(() => {
